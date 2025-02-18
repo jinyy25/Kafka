9e817c42062f75c27148f49d8c75b5adc1abdc46
jinyy25 <jinyy2593@gmail.com>
2025-02-18 13:31:31 +0900

    second

---
+++ src/main/java/com/example/kafkabasictest/KafkaBasicTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkabasictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBasicTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaBasicTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kafkabasictest/KafkaBasicTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkabasictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaBasicTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
